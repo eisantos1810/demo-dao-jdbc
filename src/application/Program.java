5d0341a91dc7044d31bb5b0ed5b6f4ccebec455b
Edson Irineu <eisantos1810@gmail.com>
2020-07-03 12:13:43 -0300

    Delete implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -49,6 +49,8 @@ public class Program {
 		System.out.println("Enter id for delete");
 		int id = sc.nextInt();
     	sellerDao.deleteById(id);
-        System.out.println("Delete completed");        
+        System.out.println("Delete completed");   
+        
+        sc.close();
 	}
 }
--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -101,7 +101,12 @@ public class SellerDaoJDBC implements SellerDao {
 			
 			st.setInt(1, id);
 			
-			st.executeUpdate();
+			int rows = st.executeUpdate();
+			
+			if (rows == 0) {
+		//		throw new DbException("Delete not found id: " + id);
+				System.out.println("Delete not found id: " + id);
+			}
 		}
 		catch (SQLException e) {
 			throw new DbException(e.getMessage());
